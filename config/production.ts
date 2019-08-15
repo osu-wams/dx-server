3737cf0ecc2984fdaaab59a5655bd78fdd67754b
Josh Gum <revgum@gmail.com>
2019-08-15 11:01:46 -0700

    fixes canvas env configurations on production

--- config/production.ts
+++ config/production.ts
@@ -3,13 +3,6 @@ export default {
   osuApi: {
     baseUrl: 'https://api.oregonstate.edu/v1'
   },
-  canvasApi: {
-    baseUrl: 'https://oregonstate.instructure.com/api/v1'
-  },
-  canvasOauth: {
-    authUrl: 'https://oregonstate.instructure.com/login/oauth2/auth',
-    tokenUrl: 'https://oregonstate.instructure.com/login/oauth2/token'
-  },
   raveApi: {
     baseUrl: 'https://www.getrave.com/rss/oregonstate/channel2'
   },
