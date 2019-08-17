b4787ecc37493eab6e4341a8233872d0fd2d23d2
Josh Gum <revgum@gmail.com>
2019-08-16 21:09:58 -0700

    adds coverage for masquerade api

---
+++ src/api/__tests__/masquerade.test.ts
@@ -0,0 +1,42 @@
+import supertest from 'supertest';
+import app from '../../index';
+
+let request: supertest.SuperTest<supertest.Test>;
+
+beforeAll(async () => {
+  request = supertest.agent(app);
+});
+
+describe('/masquerade', () => {
+  beforeEach(async () => {
+    // Authenticate before each request
+    await request.get('/login');
+  });
+
+  it('starts and stops masquerade session', async () => {
+    await request
+      .post('/api/masquerade')
+      .send({ masqueradeId: 123123 })
+      .expect(200, 'Masquerade session started.');
+    await request.post('/api/masquerade').expect(200, 'Masquerade session ended.');
+  });
+
+  it('fails to start a session without a masqueradeId', async () => {
+    await request
+      .post('/api/masquerade')
+      .expect(500)
+      .expect(r => r.error.text === 'No masqueradeId supplied');
+  });
+
+  it('gets a null when there is no session established', async () => {
+    await request.get('/api/masquerade').expect(200, { masqueradeId: null });
+  });
+
+  it('gets a current masquerade session', async () => {
+    await request
+      .post('/api/masquerade')
+      .send({ masqueradeId: 123123 })
+      .expect(200, 'Masquerade session started.');
+    await request.get('/api/masquerade').expect(200, { masqueradeId: 123123 });
+  });
+});
