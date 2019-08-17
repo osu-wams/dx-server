37fdbc7230499f7cb081da34c592637bc0e3cfa6
Josh Gum <revgum@gmail.com>
2019-08-16 18:25:30 -0700

    adds coverage for user-account module

---
+++ src/api/modules/__tests__/user-account.test.ts
@@ -0,0 +1,62 @@
+/* eslint-disable no-unused-vars */
+
+import { findOrCreateUser, updateOAuthData } from '../user-account';
+import User from '../../models/user';
+import { mockUser } from '../../models/__mocks__/user';
+
+jest.mock('../../models/user');
+const mockUserModel = User as jest.Mocked<any>;
+
+describe('User Account module', () => {
+  describe('findOrCreateUser', () => {
+    it('returns an existing user', async () => {
+      expect.assertions(2);
+      const { user, isNew } = await findOrCreateUser(mockUser);
+      expect(user.osuId).toEqual(mockUser.osuId);
+      expect(isNew).toBeFalsy();
+    });
+    it('returns a new user', async () => {
+      mockUserModel.find.mockImplementationOnce(() => Promise.resolve(null));
+      expect.assertions(2);
+      const { user, isNew } = await findOrCreateUser(mockUser);
+      expect(user.osuId).toEqual(mockUser.osuId);
+      expect(isNew).toBeTruthy();
+    });
+    it('throws an error on failure', async () => {
+      mockUserModel.find.mockImplementationOnce(() =>
+        Promise.reject(new Error('happy little accident'))
+      );
+      try {
+        const { user, isNew } = await findOrCreateUser(mockUser);
+      } catch (err) {
+        expect(err.message).toBe('happy little accident');
+      }
+    });
+  });
+  describe('updateOAuthData', () => {
+    it('returns an updated user', async () => {
+      const updatedUser = { ...mockUser, refreshToken: 'token', isCanvasOptIn: true};
+      mockUserModel.updateCanvasData.mockImplementationOnce(() => Promise.resolve(updatedUser));
+      expect.assertions(2);
+      const user = await updateOAuthData(mockUser, {
+        account: { refreshToken: 'token' },
+        isCanvasOptIn: true
+      });
+      expect(user.refreshToken).toEqual('token');
+      expect(user.isCanvasOptIn).toBeTruthy();
+    });
+    it('throws an error on failure', async () => {
+      mockUserModel.updateCanvasData.mockImplementationOnce(() =>
+        Promise.reject(new Error('happy little accident'))
+      );
+      try {
+        await updateOAuthData(mockUser, {
+          account: { refreshToken: 'token' },
+          isCanvasOptIn: true
+        });
+      } catch (err) {
+        expect(err.message).toBe('happy little accident');
+      }
+    });
+  });
+});
