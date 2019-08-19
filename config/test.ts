a6bdaf0053cd9222666c46346bfbce6b32ce4ac2
Josh Gum <revgum@gmail.com>
2019-08-19 07:03:19 -0700

    fixes circleci

--- config/test.ts
+++ config/test.ts
@@ -2,9 +2,6 @@ export default {
   env: 'test',
   canvasOauth: {
     id: '8675309',
-    secret: 'jenny',
-    callbackUrl: 'http://bogus/canvas/auth',
-    authUrl: 'https://bogus/login/oauth2/auth',
-    tokenUrl: 'https://bogus/login/oauth2/token'
+    secret: 'jenny'
   }
 };
