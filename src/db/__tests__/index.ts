77e25cf4b31de5e38e693b13c758d2f7b5d2e099
Josh Gum <revgum@gmail.com>
2019-08-19 09:52:38 -0700

    fixes db testing on CCI

--- src/db/__tests__/index.ts
+++ src/db/__tests__/index.ts
@@ -1,9 +1,12 @@
 import nock from 'nock';
-import { scan, updateItem, getItem, putItem } from '../index';
+import { scan, updateItem, getItem, putItem, DYNAMODB_ENDPOINT } from '../index';
 
 beforeEach(() => {
-  nock('http://bogus:8000')
+  nock(DYNAMODB_ENDPOINT)
     .get(/.*/)
+    .reply(200, {});
+  nock(DYNAMODB_ENDPOINT)
+    .post(/.*/)
     .reply(200, {});
 });
 
