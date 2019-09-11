a776c5f853dcfad186462416ad22e2edddd219f4
Josh Gum <revgum@gmail.com>
2019-09-11 13:07:53 -0700

    adds configurations for test environment

--- config/test.ts
+++ config/test.ts
@@ -1,12 +1,13 @@
 export default {
   env: 'test',
+  apiKeys: '[{"key":"blah","isAdmin":false}]',
   canvasOauth: {
     id: '8675309',
     secret: 'jenny'
   },
   aws: {
     dynamodb: {
-      endpoint: 'http://bogus:8000'
+      endpoint: 'http://localhost:8000'
     }
   }
 };
