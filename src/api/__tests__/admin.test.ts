30bd2019fbe38938d450b051e3deb7b8bd0d3ad6
Josh Gum <revgum@gmail.com>
2019-10-04 13:59:33 -0700

    exposes an admin function to reset the caches for external API calls

--- src/api/__tests__/admin.test.ts
+++ src/api/__tests__/admin.test.ts
@@ -1,10 +1,13 @@
 import supertest from 'supertest';
 import app from '../../index';
 import User from '../models/user';
+import cache from '../modules/cache';
+import { mockedFlushDb, mockedFlushDbResponse } from '../modules/__mocks__/cache';
 
 jest.mock('redis');
 jest.mock('../util.ts');
 jest.mock('../models/user.ts');
+jest.mock('../modules/cache.ts');
 
 let request: supertest.SuperTest<supertest.Test>;
 const mockedUser = User as jest.Mocked<any>;
@@ -19,21 +22,45 @@ describe('/admin', () => {
     await request.get('/login');
   });
 
-  it('should return success', async () => {
-    // mock a dynamodb method call that will simulate a successful process
-    mockedUser.clearAllCanvasRefreshTokens.mockImplementation(() => {
-      return [true, []];
+  describe('reset-sessions', () => {
+    it('should return success', async () => {
+      // mock a dynamodb method call that will simulate a successful process
+      mockedUser.clearAllCanvasRefreshTokens.mockImplementation(() => {
+        return [true, []];
+      });
+      await request
+        .get('/api/admin/reset-sessions')
+        .expect(200, 'Tokens reset, session cache is clearing.');
     });
-    await request
-      .get('/api/admin/reset-sessions')
-      .expect(200, 'Tokens reset, session cache is clearing.');
+
+    it('should return an error', async () => {
+      // mock a dynamodb method call that will simulate an error to handle
+      mockedUser.clearAllCanvasRefreshTokens.mockImplementation(() => {
+        return [false, []];
+      });
+      await request.get('/api/admin/reset-sessions').expect(500, 'Error while resetting sessions.');
+    });
   });
 
-  it('should return an error', async () => {
-    // mock a dynamodb method call that will simulate an error to handle
-    mockedUser.clearAllCanvasRefreshTokens.mockImplementation(() => {
-      return [false, []];
+  describe('reset-api-cache', () => {
+    it('should return success', async () => {
+      mockedFlushDbResponse.mockReturnValue(true);
+      cache.flushDb = mockedFlushDb;
+      await request.get('/api/admin/reset-api-cache').expect(200, 'Api cache is resetting.');
     });
-    await request.get('/api/admin/reset-sessions').expect(500, 'Error while resetting sessions.');
+    it('should return unsuccessful', async () => {
+      mockedFlushDbResponse.mockReturnValue(false);
+      cache.flushDb = mockedFlushDb;
+      await request.get('/api/admin/reset-api-cache').expect(304);
+    });
+    it('should return an error', async () => {
+      mockedFlushDbResponse.mockReturnValue(undefined);
+      cache.flushDb = () => {
+        throw new Error();
+      };
+      await request
+        .get('/api/admin/reset-api-cache')
+        .expect(500, 'Error while resetting api cache.');
+    });
   });
 });
