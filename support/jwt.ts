d6c74e1daa7ee66cdfedaca1415bc29d76c58489
Josh Gum <revgum@gmail.com>
2020-02-07 07:05:16 -0800

    directly inspects a token without depending on cache

--- support/jwt.ts
+++ support/jwt.ts
@@ -1,13 +1,15 @@
-import { decrypt, userFromJWT } from '../src/utils/auth';
+import { decrypt, verifiedJwt } from '../src/utils/auth';
 import { ENCRYPTION_KEY, JWT_KEY } from '../src/constants';
 
 console.log('\n\nUSAGE:\nyarn ts-node support/jwt.ts <encrypted jwt token>\n\n');
 const args = process.argv.slice(2);
 const [encrypted] = args;
 const decrypted = decrypt(encrypted, ENCRYPTION_KEY, JWT_KEY);
-const user = userFromJWT(decrypted, JWT_KEY);
+const { user, iat } = verifiedJwt(decrypted, JWT_KEY);
 
 console.log('--Decrypted JWT--');
 console.log(decrypted);
-console.log('\n\n--User from JWT--');
+console.log(`\n\n--User from JWT issued at ${iat}--`);
 console.log(user);
+
+process.exit();
