9ca4577c07ca2ab8a4c779047845be260b076907
Josh Gum <revgum@gmail.com>
2020-06-04 16:27:42 -0700

    sets cache to minimum for mcm api

--- config/default.ts
+++ config/default.ts
@@ -24,7 +24,7 @@ export default {
     baseUrl: 'https://data.dx.oregonstate.edu',
   },
   dxMcmApi: {
-    cacheEndpointSec: '60',
+    cacheEndpointSec: '1',
     baseUrl: 'https://dev.mcm.oregonstate.edu',
     token: '',
   },
