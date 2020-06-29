a0715e1b4b74d9a0a5fe8cb0dd0c8933d1c3b1e9
Josh Gum <revgum@gmail.com>
2020-06-29 06:51:18 -0700

    use lib package for source of training interface

--- src/api/modules/dx.ts
+++ src/api/modules/dx.ts
@@ -433,30 +433,10 @@ export const getDxAlerts = async (): Promise<Types.Alert[]> => {
   }
 };
 
-// TODO: Move to lib
-interface Training {
-  audiences: string[];
-  id: string;
-  title: string;
-  image?: string;
-  contact?: string;
-  cost?: boolean;
-  body?: string;
-  department?: string;
-  duration?: string;
-  featured?: boolean;
-  frequency?: string;
-  prerequisites?: string;
-  courseDesign?: string;
-  tags: string[];
-  type?: string;
-  websiteUri?: string;
-  websiteTitle?: string;
-}
 /**
  * Get all trainings from DX API.
  */
-export const getTrainings = async (): Promise<Training[]> => {
+export const getTrainings = async (): Promise<Types.Training[]> => {
   try {
     const data = await fetchData(
       () =>
