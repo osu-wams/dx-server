b244385745ba37c8d2adad8728af551ab7e50c34
Josh Gum <revgum@gmail.com>
2020-07-16 12:26:14 -0700

    provides the ability for full and partial masquerade

--- src/api/masquerade.ts
+++ src/api/masquerade.ts
@@ -3,12 +3,57 @@
  */
 import { Router, Request, Response } from 'express'; // eslint-disable-line no-unused-vars
 import logger from '../logger';
+import User from './models/user';
 
 const router: Router = Router();
 
+/**
+ * It is possible to masquerade as a user who doesn't have a current Users table record (partial masquerade)
+ * or a user who does have a Users table record (full masquerade).
+ *
+ * Partial Masquerade: OSU, Canvas, and DX APIs will show contextual data for this user except for
+ * any profile related overrides (theme, primaryAffiliation, etc).
+ *
+ * Full Masquerade: OSU, Canvas and DX APIs will show full contextual data and allow for some persistent functionality.
+ * ! Important: User profile settings will be changed/persisted during full masquerade.
+ *
+ * * Limitation(s);
+ * *   - Multi-Channel Messages cannot be marked as read
+ */
 router.post('/', async (req: Request, res: Response) => {
   const { masqueradeId, masqueradeReason } = req.body;
   if (masqueradeId && masqueradeReason) {
+    const user = await User.find(masqueradeId);
+    if (user) {
+      const {
+        lastName,
+        lastLogin,
+        affiliations,
+        primaryAffiliationOverride,
+        primaryAffiliation,
+        onid,
+        audienceOverride,
+        firstName,
+        theme,
+        osuId,
+        email,
+        isCanvasOptIn,
+      } = user;
+      req.session.passport.user.masquerade = {
+        lastName,
+        lastLogin,
+        affiliations,
+        primaryAffiliationOverride,
+        primaryAffiliation,
+        onid,
+        audienceOverride,
+        firstName,
+        theme,
+        osuId,
+        email,
+        isCanvasOptIn,
+      };
+    }
     req.session.passport.user.masqueradeId = masqueradeId;
     req.session.passport.user.masqueradeReason = masqueradeReason;
     logger().info(
@@ -18,12 +63,13 @@ router.post('/', async (req: Request, res: Response) => {
         osuId: req.session.passport.user.osuId,
         email: req.session.passport.user.email,
         masqueradeId,
-        masqueradeReason
-      }
+        masqueradeReason,
+      },
     );
     res.send('Masquerade session started.');
   } else if (req.session.passport.user.masqueradeId) {
     delete req.session.passport.user.masqueradeId;
+    delete req.session.passport.user.masquerade;
     res.send('Masquerade session ended.');
   } else {
     res.status(500).send({ message: 'No masqueradeId or masqueradeReason supplied.' });
@@ -33,8 +79,9 @@ router.post('/', async (req: Request, res: Response) => {
 router.get('/', (req, res) => {
   if (req.session.passport.user.masqueradeId) {
     return res.send({
+      masquerade: req.session.passport.user.masquerade,
       masqueradeId: req.session.passport.user.masqueradeId || '',
-      masqueradeReason: req.session.passport.user.masqueradeReason || ''
+      masqueradeReason: req.session.passport.user.masqueradeReason || '',
     });
   }
   return res.send({ masqueradeId: '', masqueradeReason: '' });
---
+++ src/db/scripts/create_test_user.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-console, no-unused-vars */
+
+import User from '../../api/models/user';
+import logger from '../../logger';
+
+// Provide the ability to create a test user for localhost full masquerade
+const testUser: User = {
+  osuId: 933012345,
+  firstName: 'Bob',
+  lastName: 'Ross',
+  email: 'bob@bobross.com',
+  phone: '5551212',
+  isAdmin: false,
+  isStudent: () => false,
+  primaryAffiliation: 'employee',
+  groups: [],
+  affiliations: ['employee'],
+  onid: 'rossb',
+};
+
+User.upsert(testUser)
+  .then((_v) => {
+    logger().info('Created user.', testUser);
+  })
+  .catch((err) => console.error(err));
