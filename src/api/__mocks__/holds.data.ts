f721cebaeaaf7500963551fb5e09f514031cdbdf
kristinaxlee <50277029+kristinaxlee@users.noreply.github.com>
2021-01-06 09:35:57 -0800

    added fromDate to student holds API, hold data type, and modified tests to reflect new return value

--- src/api/__mocks__/holds.data.ts
+++ src/api/__mocks__/holds.data.ts
@@ -1,28 +1,31 @@
 export const holdsData = {
   links: {
-    self: 'bogus'
+    self: 'bogus',
   },
   data: {
     attributes: {
       holds: [
         {
           toDate: '2010-01-01',
+          fromDate: '2009-10-14',
           webDisplay: true,
-          description: 'Expired Hold'
+          description: 'Expired Hold',
         },
         {
           toDate: '2199-01-01',
+          fromDate: '2021-12-05',
           webDisplay: true,
-          description: 'Permanent Hold'
+          description: 'Permanent Hold',
         },
         {
           toDate: '2198-01-01',
+          fromDate: '2050-08-12',
           webDisplay: false,
-          description: 'Not Web Displayed'
-        }
-      ]
-    }
-  }
+          description: 'Not Web Displayed',
+        },
+      ],
+    },
+  },
 };
 
 export default holdsData;
