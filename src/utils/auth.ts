5138be2b2efdf639603aa3c08c72e6c711881074
kristinaxlee <50277029+kristinaxlee@users.noreply.github.com>
2021-02-02 11:43:34 -0800

    include the returned saml values with each error message

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -32,44 +32,51 @@ const parseSamlResult = (profile: any, done: any) => {
 
   if (!user.osuId) {
     logger().error(
-      `Saml response did not include 'osuId' required field (urn:oid:1.3.6.1.4.1.5016.2.1.2.1) value.`,
+      `Saml response did not include 'osuId' required field (urn:oid:1.3.6.1.4.1.5016.2.1.2.1) value. \n Response values: `,
+      user,
     );
   }
 
   if (!user.firstName) {
     logger().error(
-      `Saml response did not include 'firstName' required field (urn:oid:2.5.4.42) value.`,
+      `Saml response did not include 'firstName' required field (urn:oid:2.5.4.42) value. \n Response values: `,
+      user,
     );
   }
 
   if (!user.lastName) {
     logger().error(
-      `Saml response did not include 'lastName' required field (urn:oid:2.5.4.4) value.`,
+      `Saml response did not include 'lastName' required field (urn:oid:2.5.4.4) value. \n Response values: `,
+      user,
     );
   }
 
   if (!user.email) {
     logger().error(
-      `Saml response did not include 'email' required field (urn:oid:1.3.6.1.4.1.5923.1.1.1.6) value.`,
+      `Saml response did not include 'email' required field (urn:oid:1.3.6.1.4.1.5923.1.1.1.6) value. \n Response values: `,
+      user,
     );
   }
 
   if (!user.primaryAffiliation) {
     logger().error(
-      `Saml response did not include 'primaryAffiliation' required field (urn:oid:1.3.6.1.4.1.5923.1.1.1.5) value.`,
+      `Saml response did not include 'primaryAffiliation' required field (urn:oid:1.3.6.1.4.1.5923.1.1.1.5) value. \n Response values: `,
+      user,
     );
   }
 
   if (!user.affiliations || !user.affiliations.length) {
     logger().error(
-      `Saml response did not include 'affiliations' required field (urn:oid:1.3.6.1.4.1.5923.1.1.1.1) values, populating with primaryAffiliation.`,
+      `Saml response did not include 'affiliations' required field (urn:oid:1.3.6.1.4.1.5923.1.1.1.1) values, populating with primaryAffiliation.\n Response values: `,
+      user,
     );
     user.affiliations = [user.primaryAffiliation];
   }
 
   if (!user.onid) {
     logger().error(
-      `Saml response did not include 'onid' required field (urn:oid:0.9.2342.19200300.100.1.1) value.`,
+      `Saml response did not include 'onid' required field (urn:oid:0.9.2342.19200300.100.1.1) value.\n Response values: `,
+      user,
     );
   }
 
